571e2e5ab78b7e82879f414cd742846c00ea7354
Dimaasaaaa <d.shkarinov@mail.ru>
2026-07-12 12:51:38 +0300

    Работа с инициализацией бд

--- database/base.py
+++ database/base.py
@@ -5,7 +5,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 ENV_PATH = BASE_DIR / ".env"
 load_dotenv(dotenv_path=ENV_PATH)
 
