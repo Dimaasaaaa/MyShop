febf09dcfe3712d63c1b1bdd41b8390996395ac8
Dimaasaaaa <d.shkarinov@mail.ru>
2026-08-03 20:16:34 +0300

    Реализация предоставление категорий меню и товаров

--- database/utils.py
+++ database/utils.py
@@ -1,3 +1,5 @@
+from idlelib import query
+
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 from database.base import engine
@@ -41,3 +43,18 @@ def db_create_user_cart(chat_id):
             return True
     except IntegrityError:
         return False
+def db_get_all_category():
+    '''получение всех категорий'''
+    with get_session() as session:
+        query = select(Categories)
+        return session.scalars(query).all
+
+def db_get_finally_price(chat_id)
+    """Попытка установить цену"""
+    with get_session() as session:
+        query = select(func.sum(FinallyCarts.final_price)).select_from(
+            join(Carts, FinallyCarts, Carts.id == FinallyCarts.cart_id)
+            .join(Users, Users.id == Carts.user_id)
+            .where(Users.telegram == chat_id)
+        )
+        return session.execute(query).scalar()
--- keyboards/inline.py
+++ keyboards/inline.py
@@ -1,3 +1,17 @@
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from database.utils import db_get_all_category, db_get_finally_price
+
 def create_categories_menu(chat_id):
-    """Представление меню с категорями товаров"""
-    pass
+    """Предоставление меню с категориями продуктов"""
+    categories = db_get_all_category()
+    total_price = db_get_finally_price(chat_id)
+
+    builder = InlineKeyboardBuilder()
+    builder.button(text=f"Корзина заказа ({total_price if total_price else 0}₽)",
+                   callback_data="корзина заказа"
+                   )
+    [builder.button(text=categories.category_name, callback_data = f"category_{category.id}")for category in categories]
+
+    builder.adjust(2,1)
+    return builder
